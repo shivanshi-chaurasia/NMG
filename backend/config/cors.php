74d70c3878daeda1f33892e1468bb39e5f7c2f8d
Shivanshi chaurasia <2k23cys2313394@gmail.com>
2026-06-25 01:49:59 +0530

    update CORS configuration to allow Vercel origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,9 +19,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://nmg-cwbhwjugg-shivanshi3.vercel.app',
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#',
+    ],
 
     'allowed_headers' => ['*'],
 
